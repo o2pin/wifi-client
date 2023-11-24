65ba713e31a194c69a7f422fbcb605e66a4f4c0c
peter <gongteng.xinyi.li@163.com>
2023-11-24 17:07:35 +0800

    feat(wifi): airmon-ng check 检查

--- wpa_fuzz/run.sh
+++ wpa_fuzz/run.sh
@@ -1,6 +1,15 @@
+# 设置监控模式
 # sudo airmon-ng start wlan0 6
+
+# 设置网卡channel保持ap一致
 # sudo iwconfig wlan0mon channel 6
 
-sudo ~/.python3/bin/python3 wpa_fuzz/main.py wlan0mon --client-mac 00:1d:43:20:18:d4 --ssid shuimuyulin-guest --psk smyl2020 --ap-mac "5A:41:20:1D:26:ED"
-# sudo ~/.python3/bin/python3 wpa_fuzz/main.py wlan0mon
+# 检查是否有其他服务使用无线网卡, 结束会影响wifi注入的服务
+# sudo airmon-ng check
+# sudo airmon-ng check kill
+
+# 链路认证 密钥协商
+# sudo ~/.python3/bin/python3 wpa_fuzz/main.py wlan0mon --client-mac 00:1d:43:20:18:d4 --ssid shuimuyulin-guest --psk smyl2020 --ap-mac "5A:41:20:1D:26:ED"
+
+sudo ~/.python3/bin/python3 wpa_fuzz/main.py wlan0mon
 
