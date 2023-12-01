ed045ff9a629f97f59cd60c4421353102e594d37
peter <gongteng.xinyi.li@163.com>
2023-12-01 18:45:13 +0800

    fix: 安装soket依赖脚本

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 CURRENT=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 PYTHON3=${PYTHON3:-python3}
-
-cd ${CURRENT}/submodule/socket_hook_py.git &&
-    ${PYTHON3} -m pip install maturin && 
-    maturin develop --release
+set -x
+if [ -n "${COMPONENT_PYTHON3_PYPI_MIRROR}" ]; then
+    ${PYTHON3} -m pip install -i ${COMPONENT_PYTHON3_PYPI_MIRROR} -r ./requirements.txt 
+    cd ${CURRENT}/submodule/socket_hook_py.git &&
+        ${PYTHON3} -m pip install -i ${COMPONENT_PYTHON3_PYPI_MIRROR} maturin && 
+        maturin develop --release
+else
+    ${PYTHON3} -m pip install -r ./requirements.txt 
+    cd ${CURRENT}/submodule/socket_hook_py.git &&
+        ${PYTHON3} -m pip install maturin && 
+        maturin develop --release
+fi
