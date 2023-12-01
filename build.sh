9d34312be45c1b103961924d1e463c6b31ede997
peter <gongteng.xinyi.li@163.com>
2023-12-01 19:23:37 +0800

    fix: 安装soket依赖脚本

--- build.sh
+++ build.sh
@@ -4,14 +4,20 @@ set -x
 CURRENT=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 echo ${CURRENT}
 PYTHON3=${PYTHON3:-python3}
+if [ -n "${WIFI_VENV}" ]; then 
+    source ${WIFI_VENV}
+fi 
+
 if [ -n "${COMPONENT_PYTHON3_PYPI_MIRROR}" ]; then
-    ${PYTHON3} -m pip install -i ${COMPONENT_PYTHON3_PYPI_MIRROR} -r ./requirements.txt 
-    cd ${CURRENT}/submodule/socket_hook_py.git &&
-        ${PYTHON3} -m pip install -i ${COMPONENT_PYTHON3_PYPI_MIRROR} maturin && 
-        maturin develop --release
-else
+    # ${PYTHON3} -m pip install -i ${COMPONENT_PYTHON3_PYPI_MIRROR} -r ./requirements.txt 
+    # cd ${CURRENT}/submodule/socket_hook_py.git &&
+    #     ${PYTHON3} -m pip install -i ${COMPONENT_PYTHON3_PYPI_MIRROR} maturin && 
+    #     maturin develop --release
+    PYTHON3="${PYTHON3} -i ${COMPONENT_PYTHON3_PYPI_MIRROR}"
+fi
+# else
     ${PYTHON3} -m pip install -r ./requirements.txt 
     cd ${CURRENT}/submodule/socket_hook_py.git &&
         ${PYTHON3} -m pip install maturin && 
         maturin develop --release
-fi
+# fi
