489e552581c07af3d2d7d333e8e9f40d7dfe855f
peter <gongteng.xinyi.li@163.com>
2023-12-01 18:34:10 +0800

    fix: pip install socket hook py

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+CURRENT=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+PYTHON3=${PYTHON3:-python3}
 
-cd submodule/socket_hook_py.git &&
-    PYTHON3={PYTHON3:-python3} &&
+cd ${CURRENT}/submodule/socket_hook_py.git &&
     ${PYTHON3} -m pip install maturin && 
-    maturin develop --release &&
+    maturin develop --release
