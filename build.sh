0455ed53fe2bc7ae9e88abb9a14615468139d467
peter <gongteng.xinyi.li@163.com>
2023-12-01 19:43:27 +0800

    fix: 安装soket依赖脚本

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ CURRENT=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 echo ${CURRENT}
 PYTHON3=${PYTHON3:-python3}
 if [ -n "${WIFI_VENV}" ]; then 
-    source ${WIFI_VENV}
+    MATURIN="${WIFI_VENV}/maturin"
+else
+    MATURIN=maturin
 fi 
 
 if [ -n "${COMPONENT_PYTHON3_PYPI_MIRROR}" ]; then
@@ -15,4 +17,4 @@ fi
 ${PYTHON3} -m pip install -r ./requirements.txt 
 cd ${CURRENT}/submodule/socket_hook_py.git &&
     ${PYTHON3} -m pip install maturin && 
-    maturin develop --release
+    ${MATURIN} develop --release
